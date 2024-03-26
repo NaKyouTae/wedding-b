cb313a41b3a3a7c5a53a1d5bb93f2d92e4bdc06a
nakyoutae <qppk@naver.com>
2024-03-26 23:16:27 +0900

    - 수정사항 반영

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,40 @@
 import React from "react";
-import {onCall, sendSms} from "../utils/Copy";
+import {onClick, onCall, sendSms, onCopy} from "../utils/Copy";
 
 const Contact = () => {
     return (
         <div className="box">
-            <h2 data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">contact</h2>
-            <ul className="contact" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">
-                <li><h3>👰🏻‍♀️ 신부측 혼주 연락처</h3></li>
-                <li>
-                    <h4>(부) 최해철</h4>
-                    <div>
-                        <a onClick={() => sendSms('01086414957')}>문자 보내기</a>
-                        <a onClick={() => onCall('01086414957')}>전화 걸기</a>
-                    </div>
+            <h2 className="ico" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">🤍</h2>
+            <ul className="contact">
+                <li data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">
+                    <h3>👰🏻‍♀️ 신부측 혼주</h3>
+                </li>
+                <li data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">
+                    <h4>신부 아버지 최해철</h4>
+                    <ul>
+                        <li>
+                            <p>기업은행 658-024454-01-017</p>
+                            <a onClick={() => onCopy('65802445401017')}>복사하기</a>
+                        </li>
+                        <li>
+                            <button onClick={() => sendSms('01086414957')}>문자 보내기</button>
+                            <button onClick={() => onCall('01086414957')}>전화 걸기</button>
+                        </li>
+                    </ul>
                 </li>
                 <li>
-                    <h4>(모) 이현정</h4>
+                    <h4>신부 어머니 이현정</h4>
+                    <ul>
+                        <li>
+                            <p>농협은행 813061-56-017909</p>
+                            <a onClick={() => onCopy('81306156017909')}>복사하기</a>
+                        </li>
+                        <li>
+                            <button onClick={() => sendSms('01048171423')}>문자 보내기</button>
+                            <button onClick={() => onCall('01048171423')}>전화 걸기</button>
+                        </li>
+                    </ul>
                     <div>
-                        <a onClick={() => sendSms('01048171423')}>문자 보내기</a>
-                        <a onClick={() => onCall('01048171423')}>전화 걸기</a>
                     </div>
                 </li>
             </ul>
