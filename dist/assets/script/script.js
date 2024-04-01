05cc8056c76473c7b3ff007c2a378a98f3ad2b20
PAP-Nakyoutae <kyoutae@chicment.com>
2024-04-02 00:31:35 +0900

    update

--- dist/assets/script/script.js
+++ dist/assets/script/script.js
@@ -128,17 +128,19 @@ $(document).ready(function() {
 
 // copy
 function onCopy(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        var $toast = $('#toast');
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+        navigator.clipboard.writeText(text).then(() => {
+            var $toast = $('#toast');
 
-        if(!$toast.hasClass('active')) {
-            $toast.addClass('active');
-        }
+            if(!$toast.hasClass('active')) {
+                $toast.addClass('active');
+            }
 
-        setTimeout(() => {
-            $toast.removeClass('active');
-        }, 1500);
-    });
+            setTimeout(() => {
+                $toast.removeClass('active');
+            }, 1500);
+        });
+    }
 }
 
 // call
@@ -148,7 +150,17 @@ function onCall(number) {
 
 // massage
 function sendSms(number) {
-    window.open(`sms:${number}&body=`)
+    const userAgent = navigator.userAgent;
+
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+        console.log("iOS 디바이스입니다.");
+        // iOS 디바이스에 대한 코드를 여기에 작성
+        window.open(`sms:${number}&body=`)
+    } else {
+        console.log("다른 유형의 디바이스입니다.");
+        // 기타 디바이스에 대한 처리를 여기에 작성
+        window.open(`sms:${number}?body=`)
+    }
 }
 
 // naver map, tmap
